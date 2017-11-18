5e97743ee7c2236c077e81ac322f1bb97b2947b5
Sunpy <jonas_steinsvik@hotmail.no>
2017-11-18 04:52:08 +0100

    Testing case

--- sample.py
+++ sample.py
@@ -13,7 +13,20 @@ if len(sys.argv) >= 3:
     mods = int(sys.argv[2])
 else:
     mods = 0
-difficulty = Difficulty(beatmap, mods)
 
-print("Stars: {}".format(difficulty.starRating))
-print("PP: {}".format(calculatePP(difficulty, 1, 300, 0)))
+difficulty = Difficulty(beatmap, 0)
+print("Calculation:")
+print("Stars: {}, PP: {}, MaxCombo: {}\n".format(difficulty.starRating, calculatePP(difficulty, 1, beatmap.GetObjectCount(), 0), beatmap.GetObjectCount()))
+
+print("Wanted values:")
+print("Stars: 7.4, PP: 784.33, MaxCombo: 2806")
+print("HitObjects: 2351, SliderTicks: 455")
+
+"""
+m = {"NOMOD": 0, "EASY": 2, "HIDDEN": 8, "HARDROCK": 16, "DOUBLETIME": 64, "HALFTIME": 256, "FLASHLIGHT": 1024}
+for key in m.keys():
+    difficulty = Difficulty(beatmap, m[key])
+    print("Mods: {}".format(key))
+    print("Stars: {}".format(difficulty.starRating))
+    print("PP: {}\n".format(calculatePP(difficulty, 1, beatmap.GetObjectCount(), 0)))
+"""

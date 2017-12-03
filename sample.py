b0392810991b7500bb1f4df7bce8c031b6a94275
Sunpy <jonas_steinsvik@hotmail.no>
2017-12-03 03:13:27 +0100

    Use mods variable...

--- sample.py
+++ sample.py
@@ -14,19 +14,15 @@ if len(sys.argv) >= 3:
 else:
     mods = 0
 
-difficulty = Difficulty(beatmap, 0)
+difficulty = Difficulty(beatmap, mods)
 print("Calculation:")
 print("Stars: {}, PP: {}, MaxCombo: {}\n".format(difficulty.star_rating, calculate_pp(difficulty, 1, beatmap.max_combo, 0), beatmap.max_combo))
-
-print("Wanted values:")
-print("Stars: 7.4, PP: 784.33, MaxCombo: 2806")
-print("HitObjects: 2351, SliderTicks: 455")
 
 """
 m = {"NOMOD": 0, "EASY": 2, "HIDDEN": 8, "HARDROCK": 16, "DOUBLETIME": 64, "HALFTIME": 256, "FLASHLIGHT": 1024}
 for key in m.keys():
     difficulty = Difficulty(beatmap, m[key])
     print("Mods: {}".format(key))
-    print("Stars: {}".format(difficulty.starRating))
-    print("PP: {}\n".format(calculatePP(difficulty, 1, beatmap.GetObjectCount(), 0)))
+    print("Stars: {}".format(difficulty.star_rating))
+    print("PP: {}\n".format(calculate_pp(difficulty, 1, beatmap.max_combo, 0)))
 """
